a395b4155b935f6bf5d21cdd710cf276995af474
Patryk Wlazłowicz <patrykwlazlowicz@icloud.com>
2020-04-05 17:25:35 +0200

    Create Resources Label Component

---
+++ arcomage/src/app/ui/game/game.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResourcesLabelComponent } from './resources-label/resources-label.component';
+
+
+
+@NgModule({
+  declarations: [ResourcesLabelComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GameModule { }
